6db70be48ad37698a067befff4e71a5b1f77182c
wwmairs <wwmairs@gmail.com>
2019-06-30 19:19:30 -0600

    all hooked up

--- heavylifting.js
+++ heavylifting.js
@@ -4,13 +4,13 @@ if (cmsData == null) {
     // retrieve it, store in session,
     // Your web app's Firebase configuration
     var firebaseConfig = {
-      apiKey: "AIzaSyAf6ZlVwUAhrc1pUMHC3dsKHi03pXf6lCY",
-      authDomain: "wwmairs-cms.firebaseapp.com",
-      databaseURL: "https://wwmairs-cms.firebaseio.com",
-      projectId: "wwmairs-cms",
-      storageBucket: "wwmairs-cms.appspot.com",
-      messagingSenderId: "1069213946397",
-      appId: "1:1069213946397:web:9b0d59d93dd81b5c"
+        apiKey: "AIzaSyCW6Yml8lX_nWnOY4Zd6o8_RFe3eXGan0s",
+        authDomain: "davydov-cms.firebaseapp.com",
+        databaseURL: "https://davydov-cms.firebaseio.com",
+        projectId: "davydov-cms",
+        storageBucket: "",
+        messagingSenderId: "97724532136",
+        appId: "1:97724532136:web:377a51561c4de231"
     };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
@@ -19,27 +19,33 @@ if (cmsData == null) {
     db.ref("data").once("value")
         .then((snapshot) => {
             snapshot.val().map((o) => {
-                dbData[o.key] = o;
-                window.sessionStorage.setItem("cmsData", JSON.stringify(dbData));
-                cmsData = dbData;
-                // do shit with the data
-                makeGalleries(cmsData);
+                if (dbData[o.category] == undefined) {
+                    dbData[o.category] = [];
+                }
+                dbData[o.category].push(o);
             });
+            window.sessionStorage.setItem("cmsData", JSON.stringify(dbData));
+            cmsData = dbData;
+            // do shit with the data
+            makeGalleries(cmsData);
+            
+            console.log("fetching...");
+            console.log(dbData);
         });
 } else {
     // do shit
     cmsData = JSON.parse(cmsData);
-    makeGalleries(cmsData);
 }
 
 function makeGalleries(cmsData) {
+    console.log(cmsData);
     for (key in cmsData) {
         // if it's a gallery
         // if you can find a gallery el defined
-        if (! cmsData[key]["is_gallery"] || ! document.getElementById(key)) {
-            console.log("skipping", key);
-            continue;
-        }
+        //if (! cmsData[key]["is_gallery"] || ! document.getElementById(key)) {
+        //    console.log("skipping", key);
+        //    continue;
+        //}
         let gallery = document.getElementById(key);
         cmsData[key]["links"].map((src) => {
             let div = document.createElement("div");
